d7ce5c54b8e42238d6d6d8d87ea784f6fa4aad0c
piy-ushk <166232912+piy-ushk@users.noreply.github.com>
2024-10-15 19:05:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
   
 // Constants
 const API_KEY = 'AIzaSyCX-s4eUSP5dQzmXao8RskFT6ZBPNhP9zE';
-const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent';
+const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=...';
 
 // DOM Elements
 const chatMessages = document.getElementById('chat-messages');
